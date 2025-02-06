e7ed4dd122e2b4befaccdd25ea12fcc1fed1bc3b
Etienne Camenen <etienne.camenen@gmail.com>
2025-02-06 19:11:03 +0100

    Add required packages

--- R/GimmeMyOmics-package.R
+++ R/GimmeMyOmics-package.R
@@ -1,4 +1,8 @@
-#' @import utils
+#' @import AnnotationHub ComplexHeatmap DESeq2 GimmeMyCluster GimmeMyPlot GenomicRanges Seurat Signac clustree cowplot e1071 enrichplot ggplot2 ggrepel irlba janitor kableExtra org.Hs.eg.db rlist RColorBrewer scales stringi tidyverse tximport utils
+#' @rawNamespace import(biomaRt, except = select)
+#' @rawNamespace import(clusterProfiler, except = rename)
+#' @rawNamespace import(reshape, except = stamp)
+#' @rawNamespace import(rstatix, except = make_clean_names)
 #' @importFrom magrittr %>%
 #' @keywords internal
 "_PACKAGE"
