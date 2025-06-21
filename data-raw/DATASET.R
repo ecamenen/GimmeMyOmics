76c041191a58a308ab7ed99866f443f282f84e43
Etienne Camenen <etienne.camenen@gmail.com>
2025-06-21 11:10:05 +0200

    Update DATASET

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,12 +3,17 @@
 id <- c("GSM8211616", "GSM8211617", "GSM8211618", "GSM8211619", "GSM8211620",
         "GSM8211621", "GSM8211622", "GSM8211623", "GSM8211624", "GSM8211625",
         "GSM8211626", "GSM8211627", "GSM8211628", "GSM8211629")
-metadata <- tibble(id, condition = rep(c("mTNBC", "HD"), each = 7))
+metadata <- data.frame(id, condition = rep(c("mTNBC", "HD"), each = 7)) %>%
+    mutate(across(everything(), as.factor)) %>%
+    column_to_rownames("id")
+metadata$condition <- relevel(metadata_samples$condition, ref = "HD")
 gene_counts <- select(gene_counts, seq(15))
 colnames(gene_counts)[2:15] <- id
 
 tnbc_neutrophils <- list(gene_counts = gene_counts, metadata = metadata)
 use_data(tnbc_neutrophils, overwrite = TRUE)
+
+---
 
 # Biomart
 genome_species <- c("hsapiens", "mmusculus")
@@ -33,3 +38,27 @@ biomart_annotation <- map(
     set_names(paste(genome_species, genome_version, sep = "_"))
 
 use_data(biomart_annotation, overwrite = TRUE)
+
+---
+  
+surface_genes <- map(
+    genome_species,
+    ~ {
+      db_annot <- useEnsembl(
+        biomart = "genes",
+        dataset = paste0(., "_gene_ensembl"),
+        version = genome_version
+      )
+      
+      getBM(
+        attributes = c("hgnc_symbol", "ensembl_gene_id", "go_id", "name_1006", "definition_1006"),
+        filters = "go",
+        values = c("GO:0005887", "GO:0009986"),
+        mart = ensembl
+      ) %>%
+        pull(ensembl_gene_id) %>%
+        unique()
+}) %>%
+  set_names(paste(genome_species, genome_version, sep = "_"))
+
+use_data(surface_genes, overwrite = TRUE)
