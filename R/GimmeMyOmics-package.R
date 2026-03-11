8156c7f56d3102ed87981cbf34cbb4065e45cf8b
Etienne Camenen <etienne.camenen@gmail.com>
2026-03-11 10:04:14 +0100

    Update DESCRIPTION and conf
    * Add updated man
    * Update packages list
    * Add Bioc to ignore

--- R/GimmeMyOmics-package.R
+++ R/GimmeMyOmics-package.R
@@ -1,5 +1,6 @@
-#' @import dplyr GimmeMyCluster GimmeMyPlot e1071 enrichplot ggplot2 ggraph ggrepel irlba magrittr
-#' @import pheatmap rlist RColorBrewer scales sf stringi stringr tidyverse utils
+#' @import dplyr e1071 enrichplot GimmeMyCluster GimmeMyPlot ggplot2 ggraph ggrepel irlba magrittr
+#' @import patchwork pheatmap RColorBrewer rlist scales sf stringi stringr tidyverse
+#' @import utils
 #' @rawNamespace import(DESeq2, except = desc)
 #' @rawNamespace import(clusterProfiler, except = c(rename, mutate))
 #' @rawNamespace import(reshape, except = c(stamp, rename))
